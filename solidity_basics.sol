c5fdc898c497964900dbc295d4405345b4e3c765
Davide Pasini <74866050+davidpasini@users.noreply.github.com>
2021-05-23 14:12:36 +0200

    Add files via upload

---
+++ solidity_basics.sol
@@ -0,0 +1,220 @@
+/* --- BASIC SYNTAX OF A SOL FILE --- */
+
+// import Solidity
+// the ^ sign indicates to import that version or higher
+pragma solidity ^0.4.0;
+
+
+// import other files
+import "filename";
+import * as symbolname from "filename";
+import {symbol1 as alias, symbol2} from "filename";
+
+
+// contract definition
+contract SimpleContract {
+  // code here
+  // variables and functions are defined within the contract code
+}
+
+
+/* --- RECAP ---
+1. Import Solidity + version
+2. Import needed files
+3. Set up the contract and the code within */
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- BASIC SYNTAX OF A CONTRACT --- */
+
+contract SimpleContract {
+  // state variable
+  // type + variable name + value assignment (optional)
+  uint storedData;
+  uint storedData = 34;
+
+  // modifier
+  // a modifier is a condition to a function or before we run a functions
+  // a modifier usually needs a funtion
+  modifier onlyData() {
+    // set condition/s of the modifier here
+    require(
+      storedData >= 0);
+      _; // this _; syntax is used to close the modifier
+  }
+
+  // function definition
+  // keyword + function name + input variables(including their type) + public/private
+  // a private function is accessible only by the contract it belongs to. A public function can also be accessed from other contracts
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  // event
+  event Sent(address from, address to, uint storedData);
+}
+
+
+/-------------------------------------------------------------------------------
+
+
+/* --- TYPES IN SOLIDITY --- */
+
+// in solidity each variable type must be specified
+// all variable can either be only declared (type + variable name) or declared and initialized (type + variable name + value)
+
+// string
+string name:
+string name = "davide";
+
+//integers
+uint storedData;
+uint storedData = 34;
+
+// boolean
+bool atrueorfalsevalue;
+bool atrueorfalsevalue = false;
+
+// address
+address walletAddress;
+address walletAddress = 0x72ba773893b;
+
+// array
+// arrays can be of strings or integers, so the type of array nees to be defined as well
+string[] names;
+uint[] integers;
+
+// bytes
+// bytes can be declared dynamically
+byte ...
+// or they can be declared specifying the number of characters or bytes
+bytes32 code;
+
+// struct
+// to create and declare your own type, you do so with 'struct'
+struct User {
+  string fisrtName;
+  string lastName;
+}
+
+// enums
+enum userType {buyer, seller};
+
+// mappings
+// mappings are like dictionaries in Python: key/value pairs structures
+// type + (address => uint) + public/private + variable name
+mapping(address => uint) public balances;
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- UNITS AND GLOBAL VARIABLES --- */
+
+contract Simple {
+  // ether sazbo finnery wei
+  bool isEqual -= (1 ether == 5000 finney);
+
+  // time units (always plural)
+  // seconds minutes hours days weeks
+  bool isTime = (1 hours == 60 minutes);
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- OTHER SPECIAL VARIABLES --- */
+
+// 'block' refers to the actual block that compose the Ethereum blockchain
+// within the block you have access to a certain number of information. See documentation for full list
+
+contract Simple {
+  // block
+  block.number;
+  block.difficulty;
+  block.coinbase ();
+
+  // message
+  // messages are what is sent to the contract
+  msg.sender;
+  msg.value;
+  msg.data;
+
+  // transaction
+  // when a transaction is completed, you have access to this type of information
+  tx.origin;
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- FUNCTIONS --- */
+
+// functions in solidity can take multiple parameters as inputs as well as return multiple parameters simultaneously
+/* Pure functions do not read or modify the state variables, which returns the
+values only using the parameters passed to the function or local variables present in it. */
+
+contract Simple {
+  function calcs(uint _a, uint _b) public pure returns (uint o_sum, uint o_product) {
+    o_sum = _a + _b;
+    o_product = _a * _b;
+  }
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- OPERATORS --- */
+
+contract Simple {
+  // Arithmetic operators + - * / % **
+  uint a = 2;
+  uint b = 3;
+
+  uint c = a ** b;
+
+  // logical operators
+  // ！ && || == !=
+  bool hasMoney = !false;
+
+  // bitwise operators
+  // & | ^ ~ >> <<
+  bytes1 contractValue = 0x02 | 0x01;
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+/* --- CONDITIONAL STATEMENTS --- */
+
+contract Simple {
+  // IF - IF/ELSE statement
+  if (a == b) {
+    // code if condition met
+  } else {
+    // run code
+  }
+
+  // WHILE loop
+  // checking condition first
+  while (a >= 0) {
+    // code if condition met
+  }
+
+  // DO/WHILE loop
+  // run do code until condition is no longer true
+  do {
+    // do this code first
+  } while (a >= 0)
+
+  // FOR loop
+  for (uint i=0; i>=50; i++) {
+    // code
+  }
+}
