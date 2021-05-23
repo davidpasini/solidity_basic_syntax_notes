5bbd11b077002d453a02bf47ceac1ea8dd3fcea7
Davide Pasini <74866050+davidpasini@users.noreply.github.com>
2021-05-23 14:44:02 +0200

    Add files via upload

---
+++ inheritance.sol
@@ -0,0 +1,46 @@
+// import solidity
+pragma solidity ^0.4.0;
+
+contract Inheritance {
+  address owner;
+  bool deceased;
+  uint money;
+
+  constructor() public payable {
+    owner = msg.sender;
+    money = msg.value;
+    deceased = false;
+  }
+
+  modifier oneOwner {
+    require (msg.sender == owner);
+    _;
+  }
+
+  modifier isDeceased {
+    require (deceased == true);
+    _;
+  }
+
+  address[] wallets;
+
+  mapping (address => uint) inheritance;
+
+  // requires modifier oneOwner to run
+  function setup(address _wallet, uint _inheritance) public oneOwner {
+    wallets.push(_wallet);
+    inheritance[_wallet] = _inheritance;
+  }
+
+  // requires modifier isDeceased to run
+  function moneyPaid() private isDeceased {
+    for(uint i=0; i<wallets.length; i++) {
+      wallets[i].transfer(inheritance[wallets[i]]);
+    }
+  }
+
+  function died() public oneOwner {
+    deceased = true;
+    moneyPaid();
+  }
+}
